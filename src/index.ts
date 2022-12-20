9155c701c9c0703bc96a9cbfcbf36bfee98d8537
Oryngali <oryngali96@gmail.com>
2022-12-20 22:42:56 +0500

    feat: add home page style and responsive design

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
 import './style.scss';
 import App from './components/app/app';
+import logo from './assets/coffee-bean-icon.svg';
+
+(document.querySelector('.logo img') as HTMLImageElement).src = logo;
 
 const app = new App();
 app.start();
